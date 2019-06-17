723ef89e53b6e62a2bb8b00a8031c1487f368c41
Alexis Espinoza <alexis.espinoza@ug.uchile.cl>
2019-06-16 22:49:42 -0400

    Implemented top-k-q

--- src/suffix_tree/SuffixTree.java
+++ src/suffix_tree/SuffixTree.java
@@ -1,5 +1,9 @@
 package suffix_tree;
 
+import utils.SortablePair;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Stack;
 
@@ -29,6 +33,23 @@ public class SuffixTree {
 			output[i++] = node.getValue();
 		}
 		return output;
+	}
+
+	public String[] top_K_Q(int k, int q) {
+		String[] result = new String[k];
+		// Get all the strings of length q with their number of appearances
+		ArrayList<SortablePair> stringsDepthQ = this.root.getStrTimesByLenght(q, "");
+		// Turn to array, to be able to sort in-place and in parallel, if possible
+		// https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Arrays.html#parallelSort(T[])
+		SortablePair[] strsDepthQ = new SortablePair[stringsDepthQ.size()];
+		stringsDepthQ.toArray(strsDepthQ);
+		// Uses parallelSort to sort the array for the number of appearances ascending
+		Arrays.parallelSort(strsDepthQ);
+		// Copies the strings in the last elements of the array
+		for (int i = 0; i < k; i++) {
+			result[i] = strsDepthQ[strsDepthQ.length - i - 1].getSecond();
+		}
+		return result;
 	}
 
 	char getChar(int index) {
